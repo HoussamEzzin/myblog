bebc875c5be4a837d6c875f33c7091afd4dfb29c
Ezzin Houssam <ezzinhoussam@gmail.com>
2021-10-09 22:25:22 +0100

    initial commit

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
-
-
-# just for testing : 
-posts = [
-    {
-        'author' : 'Ezzin',
-        'title' : 'first post',
-        'content': 'this is my first post',
-        'date_posted': 'October 07, 2021'
-    },
-    {
-        'author' : 'Ezzin',
-        'title' : 'second post',
-        'content': 'this is my second post',
-        'date_posted': 'October 08, 2021'
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts' : posts,
+        'posts' : Post.objects.all(),
         'title': 'HomePage'
     }
     return render(request, 'blog/home.html', context)
